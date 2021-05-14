b83d925b1455bcc673ce7b61c7bac71d5898f06a
CodeCasterNL <github@codecaster.nl>
2021-05-14 16:25:20 +0200

    Add Dutch translations

---
+++ translations/nl.php
@@ -0,0 +1,31 @@
+<?php 
+
+global $_MODULE;
+$_MODULE = array();
+
+// Globally used strings
+$_MODULE['<{boekuwzending}prestashop>boekuwzending_327ba858dbb1def0a5964ad8a327728a'] = 'Boekuwzending';
+$_MODULE['<{boekuwzending}prestashop>boekuwzending_9299d86eac7f11994bb0cc0431fb8514'] = 'Boekuwzending.com';
+$_MODULE['<{boekuwzending}prestashop>boekuwzending_f5b9251cc524415c42165a0169c2ae83'] = 'Verzend geplaatste orders automatisch naar uw Boekuwzending.com-account.';
+
+// Configuration values
+$_MODULE['<{boekuwzending}prestashop>configure_ea61efc17077f475e51764c3fc9df24a'] = 'Boekuwzending.com verzendmodule';
+$_MODULE['<{boekuwzending}prestashop>configure_1e9793372c986158b83b847f15fe83df'] = 'Bekijk de documentatie op de Boekuwzending.com-documentatiesite';
+$_MODULE['<{boekuwzending}prestashop>configure_f4f70727dc34561dfde1a3c529b6205c'] = 'Instellingen';
+$_MODULE['<{boekuwzending}prestashop>configure_cebd5bbe0ffdecc270a8a324e5a277dd'] = 'Live-modus';
+$_MODULE['<{boekuwzending}prestashop>configure_ea9df7a306e2f8b5af37b67084d0c984'] = 'Gebruik deze module in live-modus';
+$_MODULE['<{boekuwzending}prestashop>configure_76525f0f34b48475e5ca33f71d296f3b'] = 'Client ID';
+$_MODULE['<{boekuwzending}prestashop>configure_734082edf44417dd19cc65943aa65c36'] = 'Client Secret';
+$_MODULE['<{boekuwzending}prestashop>configure_ef58b487f320e220f91edc59140d01de'] = 'Verzendkosten zonder matrix';
+
+// Admin order detail
+$_MODULE['<{boekuwzending}prestashop>adminordertabcontent_1218caa99c5210e89e89f6c92b0512c2'] = 'Datum aangemaakt';
+$_MODULE['<{boekuwzending}prestashop>adminordertabcontent_97e7c9a7d06eac006a28bf05467fcc8b'] = 'Link';
+$_MODULE['<{boekuwzending}prestashop>adminordertabcontent_cb8fe83175bea4ba19d633b1dc6ea656'] = 'Bekijk order';
+$_MODULE['<{boekuwzending}prestashop>adminordertabcontent_e5afbdb7bbf8c34555bc67e7822d98df'] = 'Bestelling niet bekend bij Boekuwzending.';
+$_MODULE['<{boekuwzending}prestashop>adminordertabcontent_0262a3dc4fae9adc8cb46e788806ccbf'] = 'Zending aanmaken';
+$_MODULE['<{boekuwzending}prestashop>adminordertabcontent_26429fae075b2966b8110fee2bb4f678'] = 'Nieuwe zending aanmaken';
+
+$_MODULE['<{boekuwzending}prestashop>boekuwzendingordercontroller_095322390dda63ae6f6ef576555d23bc'] = 'Aanmaken van zending bij Boekuwzending mislukt: %exType%: %exMessage%';
+$_MODULE['<{boekuwzending}prestashop>boekuwzendingordercontroller_61a8a703d1d78c85d20b7b8a10c19770'] = 'Zending succesvol aangemaakt bij Boekuwzending.';
+$_MODULE['<{boekuwzending}prestashop>boekuwzendingordercontroller_696173857cf31d11d18c76fb6a1ffcfd'] = 'Zending kon niet worden aangemaakt bij Boekuwzending.';
